03ecfd9b1a623466a788f92b9226f6593d0d5c1f
teenage1ove <kirillstars7@gmail.com>
2024-06-01 19:12:52 +0300

    Add project

---
+++ redux-project/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <h1 className='font-bold'>
+      Привет
+    </h1>
+  )
+}
+
+export default App
---
+++ redux-project/src/pages/FavoritesPage.tsx
@@ -0,0 +1,5 @@
+function FavoritesPage() {
+    return (<h1>FavoritesPage</h1>);
+}
+
+export default FavoritesPage;
