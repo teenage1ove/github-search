4c41810678b8031fc9ad2340d3e426bf23b91e50
teenage1ove <kirillstars7@gmail.com>
2024-06-02 18:32:57 +0300

    Add function remove from fav

--- redux-project/src/components/RepoCard.tsx
+++ redux-project/src/components/RepoCard.tsx
@@ -21,6 +21,8 @@ function RepoCard({repo}: {repo: IRepo}) {
         setIsFav(false)
     }
 
+    
+
     return (
         <a href={repo.html_url} target='_blank'>
             <div className='border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all'>
--- redux-project/src/pages/FavoritesPage.tsx
+++ redux-project/src/pages/FavoritesPage.tsx
@@ -1,17 +1,25 @@
+import { useActions } from '../hooks/actions'
 import { useAppSelector } from '../hooks/redux'
 
 function FavoritesPage() {
     const {favorites} = useAppSelector(state => state.github)
+    const {removeFavorite} = useActions()
 
-    if (favorites.length === 0) return <p className='text-center'>No items.</p>
+    const removeFromFavorite = (f:string) => {
+        removeFavorite(f)
+    }
 
+    if (favorites.length === 0) return <p className='text-center pt-10'>No items.</p>
 
     return (
         <div className='flex justify-center pt-10 mx-auto h-screen'>
             <ul className='list-none'>
                 {favorites.map(f => (
-                    <li key={f} className='my-2 py-2 px-4 bg-slate-400 hover:underline'>
-                        <a href={f} target='_blank' className='text-white'>{f}</a>
+                    <li key={f} className='flex justify-between gap-7 items-center my-2 py-2 px-4 bg-slate-400 '>
+                        <a href={f} target='_blank' className='text-white hover:underline'>{f}</a>
+                        <button
+                        className='py-1 px-4 bg-red-400 rounded hover:shadow-md hover:bg-red-300 transition-all'
+                        onClick={() => removeFromFavorite(f.toString())}>Remove</button>
                     </li>
                 ))}
             </ul>
